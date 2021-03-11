f817787740ac1992dc318d91327aebf6d68fddd7
acervantes0512 <Acervantesc@intergrupo.com>
2021-03-11 17:19:54 -0500

    Se adiciona sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+  { path: '/dashboard', title: 'Inicio  ',  icon: 'fa fa-home', class: '' },
+  { path: '/user', title: 'Usuariossssss',  icon:'fa fa-user', class: '' },
+  { path: '/table', title: 'Proyectos',  icon:'fa fa-table', class: '' },
+  { path: '/typography', title: 'Typography',  icon:'fa fa-paragraph', class: '' },
+  { path: '/notifications', title: 'Notifications',  icon:'fa fa-envelope', class: '' },
+  { path: '/upgrade', title: 'Upgrade to PRO',  icon:'fa fa-crown', class: 'active-pro' },
+];
+
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+
+}
